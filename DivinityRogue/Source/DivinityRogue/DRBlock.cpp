0abfdf11649dc5b56bc572a3e1ce4677d1c400e9
David Björkberg <david.bjorkberg1@gmail.com>
2023-01-15 01:27:56 +0100

    Blocks dynamically create nav links

---
+++ DivinityRogue/Source/DivinityRogue/DRBlock.cpp
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DRBlock.h"
+
+#include "NavLinkCustomComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+
+// Sets default values
+ADRBlock::ADRBlock()
+{
+	mTraceLength = 600;
+	mPadding = 50;
+}
+
+void ADRBlock::BeginPlay()
+{
+	Super::BeginPlay();
+	TArray<FVector> directions = {
+		FVector(1, 0, 0),
+		FVector(0, -1, 0),
+		FVector(-1, 0, 0),
+		FVector(0, 1, 0)
+	};
+	for (int i = 0; i < directions.Num(); i++)
+	{
+		if (!CheckDirection(directions[i]))
+		{
+			CreateNavLinkInDirection(directions[i]);
+		}
+	}
+}
+
+void ADRBlock::CreateNavLinkInDirection(FVector direction)
+{
+	ANavLinkProxy* navLink = GetWorld()->SpawnActor<ANavLinkProxy>(mNavLinkClass);
+
+	FVector startPos = GetActorLocation() + FVector(0, 0, GetActorHalfHeight()*2) + direction * GetActorHalfWidth() - (mPadding * direction);
+	navLink->SetActorLocation(startPos);
+	FVector endPos = FVector(0, 0, -GetActorHalfHeight()*2) + direction * 150;
+	navLink->GetSmartLinkComp()->SetLinkData(FVector(0, 0, 0), endPos, ENavLinkDirection::BothWays);
+}
+
+bool ADRBlock::CheckDirection(FVector direction)
+{
+	FVector startPos = GetActorLocation() - FVector(0, 0, -100);
+	FVector endPos = startPos + direction * mTraceLength;
+
+	FHitResult hit;
+	return GetWorld()->LineTraceSingleByChannel(hit, startPos, endPos, ECollisionChannel::ECC_WorldStatic);
+}
+
+float ADRBlock::GetActorHalfWidth()
+{
+	return GetStaticMeshComponent()->Bounds.BoxExtent.X;
+}
+
+float ADRBlock::GetActorHalfHeight()
+{
+	return GetStaticMeshComponent()->Bounds.BoxExtent.Z;
+}
---
+++ DivinityRogue/Source/DivinityRogue/DRBlock.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "Navigation/NavLinkProxy.h"
+#include "DRBlock.generated.h"
+
+UCLASS()
+class DIVINITYROGUE_API ADRBlock : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+	ADRBlock();
+	virtual void BeginPlay() override;
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<ANavLinkProxy> mNavLinkClass;
+private:
+	void CreateNavLinkInDirection(FVector direction);
+	bool CheckDirection(FVector direction);
+	float GetActorHalfWidth();
+	float GetActorHalfHeight();
+
+	float mPadding;
+	float mTraceLength;
+};
